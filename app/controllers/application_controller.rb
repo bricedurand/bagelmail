a8bbfdbec64e9a4c5678352a286ed3bb6750202e
pinouchon <pinouchon@gmail.com>
2013-05-11 19:09:18 +0200

    signin signout paths

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :authenticate_user!
+
+  private
+  def after_sign_in_path_for(arg)
+    mails_path
+  end
+
+  def after_sign_out_path_for(arg)
+    root_path
+  end
 end
