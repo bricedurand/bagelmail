23247c9d264ee59de7f36a103836fbc6f50e2b53
Benjamin Lan Sun Luk <lsl.benjamin@gmail.com>
2013-05-11 13:44:40 +0200

    Empty Rails 3 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BagelMail::Application.config.secret_token = 'e28cb35027d91657b4bbe1c0f3cde8faa44c0a4ec971e5c83e71b22a2e48831385879822760b5ce0608f5b3f84cc03aaf69ff3cdbc945e50c93303c1c9336664'
