14e9f6002f123a22a3cbba144ea25908b6aadc0a
Benjamin Lan Sun Luk <lsl.benjamin@gmail.com>
2013-05-11 18:15:56 +0200

    Ajout de la page mail

--- app/controllers/mails_controller.rb
+++ app/controllers/mails_controller.rb
@@ -3,4 +3,9 @@ class MailsController < ApplicationController
     @mails = Mail.all
     # @mails = current_user.mails
   end
+
+  def show
+    @mail = Mail.find params[:id]
+    # @mails = current_user.mails.find params[:id]
+  end
 end
